a17874aa2857796c0c17cb1c54da90d6c4c3a6e5
Fabian-Lars <fabianlars@fabianlars.de>
2023-04-24 16:39:09 +0000

    fix(upload): Don't bail on unknown file length on download, fixes #297 (#330)
    * fix(upload): Don't bail on unknown file length on download, fixes #297
    
    * typo
    
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/4788821341
    
    Co-authored-by: FabianLars <FabianLars@users.noreply.github.com>

--- dist-js/index.mjs
+++ dist-js/index.mjs
@@ -33,6 +33,10 @@ async function upload(url, filePath, progressHandler, headers) {
         headers: headers !== null && headers !== void 0 ? headers : {},
     });
 }
+/// Download file from given url.
+///
+/// Note that `filePath` currently must include the file name.
+/// Furthermore the progress events will report a total length of 0 if the server did not sent a `Content-Length` header or if the file is compressed.
 async function download(url, filePath, progressHandler, headers) {
     const ids = new Uint32Array(1);
     window.crypto.getRandomValues(ids);
