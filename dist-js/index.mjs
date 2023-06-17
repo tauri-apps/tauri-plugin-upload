ee70a51ed41124cffe40b0c73c3b795386925f5e
Lucas Fernandes Nogueira <lucas@tauri.studio>
2023-06-17 21:56:10 +0000

    feat: update to 2.0.0-alpha.10 (#441)
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/5300306799
    
    Co-authored-by: lucasfernog <lucasfernog@users.noreply.github.com>

--- dist-js/index.mjs
+++ dist-js/index.mjs
@@ -1,6 +1,8 @@
 import { Channel, invoke } from '@tauri-apps/api/tauri';
 
 // Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 async function upload(url, filePath, progressHandler, headers) {
     const ids = new Uint32Array(1);
     window.crypto.getRandomValues(ids);
